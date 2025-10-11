8f975e00cb31afe1effb0598beb2f8d122e1f106
Parseer <AlexanderEvgenDolgov@yandex.ru>
2025-10-11 23:53:06 +0500

    Правка на этапе теста

--- test/TaskTest.java
+++ test/TaskTest.java
@@ -1,11 +1,7 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
 
 
 class TaskTest {
